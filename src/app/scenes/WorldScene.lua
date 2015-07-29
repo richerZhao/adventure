27c44b99ec0d44208d3264a13c1ed0baef970625
richerZhao <richerzhao88@gmail.com>
2015-07-29 18:50:33 +0800

    add content

--- src/app/scenes/WorldScene.lua
+++ src/app/scenes/WorldScene.lua
@@ -3,7 +3,9 @@ local WorldScene = class("WorldScene", function()
 end)
 
 function WorldScene:ctor()
-	self.map = display.newTilesSprite("bgTile.png",cc.rect(0, 0, display.width * 2, display.height * 2))
+	-- self.map = display.newTilesSprite("bgTile.png",cc.rect(0, 0, display.width * 2, display.height * 2))
+	self.map = cc.TMXTiledMap:create("map.tmx")
+	self.backGround_ = self.map:getLayer("backgroundLayer")
 	self:addChild(self.map)
 	self.touchLayer = display.newLayer()
 	self:addChild(self.touchLayer)
@@ -18,20 +20,38 @@ function WorldScene:ctor()
      --    	end
      --    end
      	print("touch,event.name="..event.name)
-        return self:onTouch(event, event.x, event.y)
+        return self:onTouch(event)
     end)
     self.touchLayer:setTouchEnabled(true)
 
+
+    --添加主角
+    -- self.roleLayer = display.newLayer():addTo(self,10)
+    self.sprite = display.newSprite("Player2.png",16,208)
+	
+    self:addChild(self.sprite)
+
+
+    for i=0,99 do
+    	for j=0,99 do
+    		local gid = self.backGround_:getTileAt({i,j})
+    		if gid then
+    			print("x="..i..",y="..j.."gid="..gid)
+    		end
+    	end
+    end
+
+
     -- 创建player批渲染结点
-    self.playerNode = display.newBatchNode("player.png", 100000)
-    self:addChild(self.playerNode)
+    -- self.playerNode = display.newBatchNode("player.png", 100000)
+    -- self:addChild(self.playerNode)
 
-    display.addSpriteFrames("player.plist", "player.png")
+    -- display.addSpriteFrames("player.plist", "player.png")
 
-    local frames = display.newFrames("player_f0006_walk_1_%02d.png",1,8)
-    self.sprite = display.newSprite(frames[1], display.cx, display.cy)
-    self.sprite:setFlippedX(true)
-    self.playerNode:addChild(self.sprite, 100)
+    -- local frames = display.newFrames("player_f0006_walk_1_%02d.png",1,8)
+    -- self.sprite = display.newSprite(frames[1], display.cx, display.cy)
+    -- self.sprite:setFlippedX(true)
+    -- self.playerNode:addChild(self.sprite, 100)
 end
 
 function WorldScene:onEnter()
@@ -42,15 +62,15 @@ function WorldScene:onExit()
 
 end
 
-function WorldScene:onTouch(event,x,y)
+function WorldScene:onTouch(event)
 	-- event.name 是触摸事件的状态：began, moved, ended, cancelled
     -- event.x, event.y 是触摸点当前位置
     -- event.prevX, event.prevY 是触摸点之前的位置
 	if event.name == "moved" then
 		self.isMoved_ = true
-		print("x="..x..",y="..y..",event.prevX="..event.prevX..",event.prevY="..event.prevY)
-		local nowX = self.map:getPositionX() + event.prevX - x
-		local nowY = self.map:getPositionY() + event.prevY - y
+		print("x="..event.x..",y="..event.y..",event.prevX="..event.prevX..",event.prevY="..event.prevY)
+		local nowX = self.map:getPositionX() + event.prevX - event.x
+		local nowY = self.map:getPositionY() + event.prevY - event.y
 		if nowX >= 0 then
 			nowX = 0
 		end
@@ -73,18 +93,66 @@ function WorldScene:onTouch(event,x,y)
     elseif event.name == "ended" then
     	if self.isMoved_ then
     		self.isMoved_ = false
+    	-- elseif self.isRoleMoved_ then
+
     	else
-    		-- 创建动画
-    		local frames = display.newFrames("player_f0006_walk_1_%02d.png",1,8)
-		    local animation = display.newAnimation(frames, 1 / 8)
-		    -- 播放动画
-		    self.sprite:playAnimationForever(animation)
-    		self.sprite:moveTo(5, event.x, event.y)
+		    --如果触点在主角左边,则主角往左移,如果触点在主角右边,则主角往右移动
+		    local lengthX = event.x - self.sprite:getPositionX()
+		    local lengthY = event.y - self.sprite:getPositionY()
+		    local moveX,moveY = 0,0
+		    if math.abs(lengthX) >= math.abs(lengthY) then
+		    	if lengthX < 0 then
+		    		--向左移动一格
+		    		moveX = -32
+		    	elseif lengthX > 0 then
+		    		--向右移动一格
+		    		moveX = 32
+		    	end
+		    else
+		    	if lengthY < 0 then
+		    		--向下移动一格
+		    		moveY = -32
+		    	elseif lengthY > 0 then
+		    		--向上移动一格
+		    		moveY = 32
+		    	end
+		    end
+
+		    local moveToX = self.sprite:getPositionX() + moveX
+		    local moveToY = self.sprite:getPositionY() + moveY
+		    local tiledX = math.modf(moveToX/self.map:getTileSize().width)
+		    local tiledY = math.modf(((self.map:getMapSize().height * self.map:getTileSize().height ) - moveToY) / self.map:getTileSize().height) 
+		    print("tiledX="..tiledX.."tiledY="..tiledY)
+		    local gid = self.backGround_:getTileAt({tiledX,tiledY})
+		    dump(gid,"gid",gid)
+
+		    self:playerMoveOn(self.sprite, self.map, moveX, moveY)
+		    
     	end
 	end
 
 	return true
 end
+
+function WorldScene:playerMoveOn(player,background,x,y)
+	local isMoveRole = true
+	if x ~= 0 then
+		if player:getPositionX() >= display.width / 2 then
+			isMoveRole = false
+		end
+	elseif y ~= 0 then
+		if player:getPositionY() >= display.height / 2 then
+			isMoveRole = false
+		end
+	end
+
+	if isMoveRole then
+		player:moveBy(1, x, y)
+	else
+		background:moveBy(1, -x, -y)
+	end
+end
+
 
 return WorldScene
 
